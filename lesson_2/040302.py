172d25a631812c01bf863af98b137d37b153379a
Кабинет информатики №310 <kab310@gym11.local>
2019-03-04 18:43:49 +0700

    task#2

---
+++ lesson_2/040302.py
@@ -0,0 +1,12 @@
+from tkinter import*
+root=Tk()
+root.geometry('500x100')
+btn1=Button(root, text='Button1', height=1)
+btn1.grid(row=0)
+ent1=Entry(root)
+ent1.grid(row=0, column=2,sticky=W)
+btn2=Button(root, text='Button2', height=2)
+btn2.grid(row=2)
+ent2=Entry(root)
+ent2.grid(row=2, column=2,sticky=W)
+root.mainloop()
