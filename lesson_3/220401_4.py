58d77bfed447a61354b60021d9da4946015cabc5
Кабинет информатики №310 <kab310@gym11.local>
2019-04-22 16:25:38 +0700

    4 task

---
+++ lesson_3/220401_4.py
@@ -0,0 +1,14 @@
+from tkinter import*
+from random import*
+
+root=Tk()
+canv=Canvas(root, width=400,height=400)
+colors=['blue', 'green', 'red', 'yellow']
+for i in range(12):
+    R=randint(5,50)
+    x=randint(R,400-R)
+    y=randint(R,400-R)
+    color=choice(colors)
+    canv.create_oval(x-R,y-R, x+R, y+R, fill=color)
+canv.pack()
+mainloop()
